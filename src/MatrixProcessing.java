9a842ba88261fca0dfddf73a7b8970492f842d7d
Ivan <goncharukivan.biz@gmail.com>
2021-09-18 03:29:41 -0400

    Initial Commit. Set up two classes and added a block comment for the tasks we need to work on.

---
+++ src/MatrixProcessing.java
@@ -0,0 +1,24 @@
+
+/*
+Design a Java program to implement matrix arithmetic for square matrices (same number of rows and columns). You will
+need to use your math textbook or the Internet to review operations with matrices.
+Make sure that your program is calling methods to perform (at least) the following operations:
+    -- Generate: Generate a matrix with random values 1 - 10
+    -- Addition: See http://mathworld.wolfram.com/MatrixAddition.html
+    -- Subtraction: Figure it out!
+    -- Multiplication
+        - Multiply two matrices: See http://mathworld.wolfram.com/MatrixMultiplication.html
+        - Multiply a matrix by a scalar: Equivalent to multiplication of each element by that scalar.
+    -- Transposition: See http://mathworld.wolfram.com/Transpose.html
+    -- Matrix Trace: See http://mathworld.wolfram.com/MatrixTrace.html
+    -- Print (use printf)
+    -- Have another method called menu(), that should list the options for matrix operations for which you designed the
+    previous functions, and an option to exit the program. Your program should loop until the user chooses to exit.
+    In this loop you are required to use a switch statement for all possible cases.
+ */
+
+public class MatrixProcessing {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Modularity.java
@@ -0,0 +1,34 @@
+/*
+Write a Java program to solve the following problem using modularity:
+        -- Write a method that rotates a one-dimensional array with one position to the left in a circular
+        way, so that the first element becomes the last element (shift left). Call this method
+        shiftLeft. For example, if the array is 17 34 22 55 37 10, then after rotation the array is
+        34 22 55 37 10 17
+        -- Write a method that rotates a one-dimensional array with one position to the right in a
+        circular way, so that the last element becomes the first element (shift right). Call this method
+        shiftRight. For example, if the array is 17 34 22 55 37 10, then after rotation the array is 10
+        17 34 22 55 37
+        -- Overload the method shiftLeft so that you rotate the array left by k positions. For example,
+        if the array is 17 34 22 55 37 10 and k = 2, then after rotation the array is 22 55 37 10 17
+        34
+        -- Overload the method shiftRight so that you rotate the array right by k positions. For
+        example, if the array is 17 34 22 55 37 10 and k = 2, then after rotation the array is
+        37 10 17 34 22 55
+        -- Write a method shiftLeftK so that you rotate the array left by k positions. The method
+        should preserve the original array and save the results of the left rotation into a second array.
+        Method signature: public static int[] shiftLeftK(int[]list, int s, int k)
+        -- Write a method shiftRightK so that you rotate the array right by k positions. The method
+        should preserve the original array and save the results of the right rotation into a second
+        array. Method signature: public static int[] shiftRightK(int[]list, int s, int k)
+Your program will invoke methods to initialize the array (random values between 1 and 100) print the
+array before the rotation, rotate the array, and then print the array (or the resulting array) after
+rotation. Use dynamic arrays and ask the user for the array size. Like in the previous program, you will
+use a method called menu(), that should print a list of options , including the option to exit the
+program. Your program should loop until the user chooses to exit. In this loop you are required to use a
+switch statement for all possible cases.
+ */
+public class Modularity {
+    public static void main(String[] args) {
+
+    }
+}
